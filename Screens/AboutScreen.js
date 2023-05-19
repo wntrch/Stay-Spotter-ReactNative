aa5fa37364f31620ed4b485c28e38a63797861ef
Leo Saban <leodsaban@gmail.com>
2023-05-19 14:12:42 -0600

    about page commit

--- Screens/AboutScreen.js
+++ Screens/AboutScreen.js
@@ -1,19 +1,17 @@
-ScrollViewFix
-import { StyleSheet, Text, View, Image } from "react-native";
+// ScrollViewFix
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
-main
+// import { Text, View, Image, ScrollView } from 'react-native';
+// main
 
 const About = () => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.contentContainer}>
         <Text style={styles.heading}>
-ScrollViewFix
           Our mission is to find every family the space they need to relax,
           reconnect, and enjoy precious time away together
           Our mission is to find every family the space they need to relax, reconnect, and enjoy precious time away together
-main
         </Text>
       </View>
       <View style={styles.bannerContainer}>
@@ -42,11 +40,10 @@ main
 };
 
 const styles = StyleSheet.create({
-  container: {
-ScrollViewFix
-    flex: 1,
-    backgroundColor: "#fff",
-  },
+  // container: {
+  //   // flex: "1",
+  //   // backgroundColor: "#fff",
+  // },
   bannerContainer: {
     height: "30%",
   },
@@ -54,9 +51,9 @@ ScrollViewFix
     width: "100%",
     height: "100%",
   },
-  // contentContainer: {
-  //   paddingHorizontal: 20,
-  // },
+  contentContainer: {
+    paddingHorizontal: 20,
+  },
   heading: {
     fontSize: 24,
     fontWeight: "bold",
@@ -78,41 +75,41 @@ ScrollViewFix
     fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center',
-main
-    marginTop: 20,
-    marginBottom: 20,
+// main
+    // marginTop: 20,
+    // marginBottom: 20,
   },
   subHeading: {
     fontSize: 18,
-ScrollViewFix
-    fontWeight: "bold",
-    textAlign: "center",
-    marginTop: 20,
-    marginBottom: 20,
+// ScrollViewFix
+    // fontWeight: "bold",
+    // textAlign: "center",
+    // marginTop: 20,
+    // marginBottom: 20,
   },
-  // categoriesContainer: {
-  //   flexDirection: "row",
-  //   justifyContent: "space-around",
-  //   marginTop: 20,
-  // },
-  // categoryImage: {
-  //   width: 50,
-  //   height: 50,
-  //   borderRadius: 10,
-  // },
+  categoriesContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+  },
+  categoryImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 10,
+  },
   categoryText: {
     fontSize: 16,
     fontWeight: "bold",
     textAlign: "center",
     marginTop: 10,
   },
-=======
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: 20,
-    marginBottom: 20,
-  },
-main
+// =======
+//     fontWeight: 'bold',
+//     textAlign: 'center',
+//     marginTop: 20,
+//     marginBottom: 20,
+//   },
+// main
 });
 
 export default About;
